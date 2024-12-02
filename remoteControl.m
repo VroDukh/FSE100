fb3a63374a7d4cb06badbe2f3b65a82387ac8bf7
Prince Dahiya <pdahiya9384@gmail.com>
2024-12-02 16:46:44 -0700

    Update remoteControl.m

--- remoteControl.m
+++ remoteControl.m
@@ -1,44 +1,45 @@
-global key;
+global key; %#ok<GVMIS>
 InitKeyboard();
-brick.SetColorMode(2, 2);
+brick.SetColorMode(2, 2);  % Ensure the color sensor is set up
 
 % O - up
 % L - down
-% q - quit
+% Q - quit (returns to main script)
 
-while 1
-    pause(0.1);
+while true
+    pause(0.1);  % Pause for system stability
     switch key
-    case 'w'
-        disp('FORWARD');
-        brick.MoveMotor('B',95);
-        brick.MoveMotor('D',95);
-    case 's'
-        disp('BACKWARD');
-        brick.MoveMotor('B',-95);
-        brick.MoveMotor('D',-95);
-    case 'd'
-        disp('RIGHT');
-        brick.MoveMotor('B',95);
-        brick.MoveMotor('D',-95);
-    case 'a'
-        disp('LEFT');
-        brick.MoveMotor('B',-95);
-        brick.MoveMotor('D',95);
-    case 'o'
-        brick.MoveMotor('C', 40); 
-        disp('UP');
-    case 'l'
-        brick.MoveMotor('C', -40); 
-        disp('DOWN');
-    case 0
-        disp('No Key Pressed!');
-        brick.StopMotor('BDC','Coast');
-    case 'q'
-        brick.StopMotor('ADB', 'Brake');
-        run("FinalMazeV7_2_2.m");
-        break;
+        case 'w'
+            disp('FORWARD');
+            brick.MoveMotor('B', 95);
+            brick.MoveMotor('D', 95);
+        case 's'
+            disp('BACKWARD');
+            brick.MoveMotor('B', -95);
+            brick.MoveMotor('D', -95);
+        case 'd'
+            disp('RIGHT');
+            brick.MoveMotor('B', 95);
+            brick.MoveMotor('D', -95);
+        case 'a'
+            disp('LEFT');
+            brick.MoveMotor('B', -95);
+            brick.MoveMotor('D', 95);
+        case 'o'
+            disp('UP');
+            brick.MoveMotor('C', 40); 
+        case 'l'
+            disp('DOWN');
+            brick.MoveMotor('C', -40); 
+        case 0
+            disp('No Key Pressed!');
+            brick.StopMotor('BDC', 'Coast');
+        case 'q'
+            disp('Exiting Remote Control Mode...');
+            brick.StopMotor('BDC', 'Brake');
+            run("FinalV4.m");
+            break;
     end
 end
+
 CloseKeyboard();
-   

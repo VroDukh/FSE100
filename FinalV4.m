fbc128f2d05e59ef56eec1879e35956e5a72b489
Prince Dahiya <pdahiya9384@gmail.com>
2024-12-03 03:10:18 -0700

    Update FinalV4.m

--- FinalV4.m
+++ FinalV4.m
@@ -1,16 +1,18 @@
+%#ok<*GVMIS>
 % Connect to the EV3 brick
-brick = ConnectBrick('GROUP4');
+%brick = ConnectBrick('GROUP4');
+pause(2);
 brick.SetColorMode(2, 2);  % Set the color sensor on port 2 to color mode
 rightMotor = 'D';  % Right motor
 leftMotor = 'B';   % Left motor
-wallThreshold = 40;  % Distance threshold to detect walls 
+wallThreshold = 55;  % Distance threshold to detect walls 
 checkpointDelay = 0.5; % Time to move forward after reaching Green
 firstYellow = true;
 
 % Time for a 90-degree turn
-turnDurationL = 0.65; % Left turn
-turnDurationR = 0.63; % Right turn
-moveForwardDuration = 2; % Time to move forward after a turn
+turnDurationL = 1; % Left turn
+turnDurationR = 0.92; % Right turn
+moveForwardDuration = 1.5; % Time to move forward after a turn
 
 % Initialize keyboard control
 global key;
@@ -22,6 +24,9 @@ if isempty(finalGreen)
     finalGreen = false; % Default value
 end
 
+% brick.MoveMotor(leftMotor, 92);
+% brick.MoveMotor(rightMotor, 90);
+% pause(2);
 
 while true
     pause(0.1); % Pause for system stability
@@ -32,20 +37,21 @@ while true
     if colorCode == 5  % RED color
         brick.StopMotor('BD', 'Brake');
         pause(3);  % Stop for 3 seconds
-        brick.MoveMotor(leftMotor, 100);
-        brick.MoveMotor(rightMotor, 99);
+        brick.MoveMotor(leftMotor, 92);
+        brick.MoveMotor(rightMotor, 90);
 
     elseif colorCode == 4  % YELLOW detected
         
         %If detecting Yellow for the first time and haven't been to green yet
         if firstYellow && ~finalGreen
             disp('Starting Positiong detected, Moving Forward...');
-            brick.MoveMotor(leftMotor, 100);
-            brick.MoveMotor(rightMotor, 99);
+            brick.MoveMotor(leftMotor, 92);
+            brick.MoveMotor(rightMotor, 90);
 
             pause(2);  % Keep Moving forward for 2 seconds
 
             firstYellow = false;  % Mark that the first yellow has been handled
+            disp(['First Yellow: ', num2str(firstYellow)]);
 
         elseif finalGreen
             disp('Final Position detected, Stopping...'); 
@@ -76,43 +82,87 @@ while true
     % Check front wall or open left side
     if brick.TouchPressed(1) || leftDistance > wallThreshold
         brick.StopMotor('BD', 'Brake');
+        
+        if brick.TouchPressed(1) 
+            brick.MoveMotor('BD',-90);
+            pause(0.25);
+        end
 
         if leftDistance > wallThreshold
             % Turn left if no wall on the left
-            disp('Turning left...');
-            brick.MoveMotor(leftMotor, -100);
-            brick.MoveMotor(rightMotor, 100);
-            pause(turnDurationL);  % Turn left for the specified duration
+            % disp('Turning left...');
+            % pause(1);
+            % brick.MoveMotor(leftMotor, -90);
+            % brick.MoveMotor(rightMotor, 90);
+            % pause(turnDurationL);  % Turn left for the specified duration
 
-            %Alternate Approach to Turning LEFT
-            %brick.MoveMotorAngleRel(leftMotor, -90, 260, 'Brake'); 
-            %brick.WaitForMotor(leftMotor);
-            %brick.MoveMotorAngleRel(rightMotor,90, 260, 'Brake');
+            % brick.StopMotor('BD','Brake');
+
+            brick.MoveMotorAngleRel(leftMotor, -95, 627, 'Brake'); 
+            brick.MoveMotorAngleRel(rightMotor,95, 627, 'Brake');
+            brick.WaitForMotor(leftMotor);
+
+            colorCode = brick.ColorCode(2); 
+
+            if colorCode == 5  % RED color
+                brick.StopMotor('BD', 'Brake');
+                pause(3);  % Stop for 3 seconds
+                brick.MoveMotor(leftMotor, 92);
+                brick.MoveMotor(rightMotor, 90);
+            end
         else
             % Turn right if wall on the left
-            disp('Turning right...');
-            brick.MoveMotor(leftMotor, 100);
-            brick.MoveMotor(rightMotor, -100);
-            pause(turnDurationR);  % Turn right for the specified duration
+            % disp('Turning right...');
+            % pause(1);
+            % brick.MoveMotor(leftMotor, 80);
+            % brick.MoveMotor(rightMotor, -80);
+            % pause(turnDurationR);  % Turn right for the specified duration
 
-            %Alternate Approach to Turning RIGHT
-            %brick.MoveMotorAngleRel(leftMotor, 90, 260, 'Brake'); 
-            %brick.WaitForMotor(leftMotor);
-            %brick.MoveMotorAngleRel(rightMotor, -90, 260, 'Brake');
+            brick.MoveMotorAngleRel(leftMotor, 95, 565, 'Brake'); 
+            brick.MoveMotorAngleRel(rightMotor,-95, 605, 'Brake');
+            brick.WaitForMotor(rightMotor);
+
+            brick.StopMotor('BD','Brake');
+
+            colorCode = brick.ColorCode(2); 
+            if colorCode == 5  % RED color
+                brick.StopMotor('BD', 'Brake');
+                pause(3);  % Stop for 3 seconds
+                brick.MoveMotor(leftMotor, 92);
+                brick.MoveMotor(rightMotor, 90);
+            end
         end
-
-        brick.StopMotor('BD', 'Brake');
 
         % Move forward after a turn
         disp('Moving forward after turn...');
-        brick.MoveMotor(leftMotor, 100);
-        brick.MoveMotor(rightMotor, 99);
+        brick.MoveMotor(leftMotor, 92);
+        brick.MoveMotor(rightMotor, 90);
         pause(moveForwardDuration);  % Move forward for the specified duration
         brick.StopMotor('BD', 'Brake');
+
+        if colorCode == 5  % RED color
+            brick.StopMotor('BD', 'Brake');
+            pause(3);  % Stop for 3 seconds
+            brick.MoveMotor(leftMotor, 92);
+            brick.MoveMotor(rightMotor, 90);
+        end
+
+    elseif leftDistance < 4
+        brick.MoveMotor(leftMotor, 92);
+        brick.MoveMotor(rightMotor, -90);
+        pause(0.5);    
+    
     else
         % Move forward if no front wall
-        brick.MoveMotor(leftMotor, 100);
-        brick.MoveMotor(rightMotor, 99);
+        brick.MoveMotor(leftMotor, 92);
+        brick.MoveMotor(rightMotor, 90);
+
+        if colorCode == 5  % RED color
+            brick.StopMotor('BD', 'Brake');
+            pause(3);  % Stop for 3 seconds
+            brick.MoveMotor(leftMotor, 92);
+            brick.MoveMotor(rightMotor, 90);
+        end
     end
 
     switch key
